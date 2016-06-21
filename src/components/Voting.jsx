c2a7eacfe9e94dff2005894af818a24543f0ad13
Justin Graber <jgraberdev@gmail.com>
2016-06-21 08:27:49 -0500

    More Client Voting Test Specs

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,10 +4,16 @@ export default React.createClass({
   getPair: function() {
     return this.props.pair || [];
   },
+
+  onClick: function(entry) {
+      debugger;
+      return this.props.vote ? this.props.vote(entry) : undefined;
+  },
+
   render: function() {
     return <div className="voting">
       {this.getPair().map(entry =>
-        <button key={entry}>
+        <button key={entry} onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
         </button>
       )}
