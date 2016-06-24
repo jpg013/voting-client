971c45a76ce4b2caa4a5186cfa43190df1a7284f
Justin Graber <jgraberdev@gmail.com>
2016-06-24 17:38:15 -0600

    Results spec

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,28 @@
+
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  getVotes: function(entry) {
+      if (this.props.tally && this.props.tally.has(entry)) {
+          return this.props.tally.get(entry);
+      }
+      return 0;
+  },
+  getPair: function() {
+      return this.props.pair || [];
+  },
   render: function() {
-    return <div>Hello from results!</div>
+    return <div className="results">
+      {this.getPair().map(entry =>
+        <div key={entry} className="entry">
+          <h1>{entry}</h1>
+          <div className='voteCount'>
+              {this.getVotes(entry)}
+          </div>
+        </div>
+      )}
+    </div>;
   }
 });
